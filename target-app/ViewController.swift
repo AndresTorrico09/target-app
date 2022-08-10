c2b2cef93ca433c194605dccea18a73caf6453d3
andresleoneltorricocossio <andreslionel09@gmail.com>
2022-08-10 23:31:53 +0100

    add hello world label

--- target-app/ViewController.swift
+++ target-app/ViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textView.text = "Hello world!"
     }
-
-
+    
 }
 
