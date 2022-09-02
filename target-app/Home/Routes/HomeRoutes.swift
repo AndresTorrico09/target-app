555e50c381b0121678f7f38879d78dd591f2ba61
andresleoneltorricocossio <andreslionel09@gmail.com>
2022-09-02 17:21:14 +0100

    add home view controller, add home route

---
+++ target-app/Home/Routes/HomeRoutes.swift
@@ -0,0 +1,21 @@
+//
+//  HomeRoutes.swift
+//  target-app
+//
+//  Created by Andres Leonel Torrico Cossio on 02/09/2022.
+//
+
+import Foundation
+import UIKit
+
+enum HomeRoutes: Route {
+  case home
+
+  var screen: UIViewController {
+    switch self {
+    case .home:
+      let home = HomeViewController()
+      return home
+    }
+  }
+}
