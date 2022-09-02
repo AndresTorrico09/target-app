04c3e3d29ee9ab1d338e09df7cc06f77a203374d
andresleoneltorricocossio <andreslionel09@gmail.com>
2022-09-02 18:50:46 +0100

    fix navigator to home on success

--- target-app/Onboarding/ViewModels/SignUpViewModel.swift
+++ target-app/Onboarding/ViewModels/SignUpViewModel.swift
@@ -72,10 +72,9 @@ class SignUpViewModel {
             switch result {
             case .success:
                 self.state = .loggedIn
-                
+                AppNavigator.shared.navigate(to: HomeRoutes.home, with: .changeRoot)
             case .failure(let error):
                 self.state = .network(state: .error(error.localizedDescription))
-                AppNavigator.shared.navigate(to: HomeRoutes.home, with: .changeRoot)
             }
         }
     }
