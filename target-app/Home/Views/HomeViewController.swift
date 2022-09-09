6b54516c686a189a64aa907a5ad3abf97493d537
andresleoneltorricocossio <andreslionel09@gmail.com>
2022-09-09 16:43:42 +0100

    add setup navigation bar in home screen

--- target-app/Home/Views/HomeViewController.swift
+++ target-app/Home/Views/HomeViewController.swift
@@ -8,11 +8,32 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         applyDefaultUIConfigs()
+        setupNavigationBar()
     }
-
+    
+    func setupNavigationBar() {
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        
+        navigationItem.title = "home_title".localized
+        
+        let barLeftButtonItem = UIBarButtonItem(image: UIImage(named: "ic_home_profile"),
+                                            style: .plain,
+                                            target: self,
+                                            action: nil)
+        navigationItem.leftBarButtonItem = barLeftButtonItem
+        navigationItem.leftBarButtonItem?.tintColor = .black
+        
+        let barRightButtonItem = UIBarButtonItem(image: UIImage(named: "ic_home_chat"),
+                                            style: .plain,
+                                            target: self,
+                                            action: nil)
+        navigationItem.rightBarButtonItem = barRightButtonItem
+        navigationItem.rightBarButtonItem?.tintColor = .black
+    }
+    
 }
