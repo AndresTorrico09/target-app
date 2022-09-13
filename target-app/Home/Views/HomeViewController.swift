0a923665f744c88bdae8ad7bbfa93ae519ef4790
andresleoneltorricocossio <andreslionel09@gmail.com>
2022-09-13 20:02:09 +0100

    remove set nav bar from home vc

--- target-app/Home/Views/HomeViewController.swift
+++ target-app/Home/Views/HomeViewController.swift
@@ -38,7 +38,6 @@ class HomeViewController: UIViewController, CLLocationManagerDelegate, MKMapView
         super.viewDidLoad()
         
         applyDefaultUIConfigs()
-        setupNavigationBar()
         setupMapConstraints()
     }
     
@@ -58,26 +57,6 @@ class HomeViewController: UIViewController, CLLocationManagerDelegate, MKMapView
         if let coor = mapView.userLocation.location?.coordinate{
             mapView.setCenter(coor, animated: true)
         }
-    }
-    
-    func setupNavigationBar() {
-        navigationController?.setNavigationBarHidden(false, animated: true)
-        
-        navigationItem.title = "home_title".localized
-        
-        let barLeftButtonItem = UIBarButtonItem(image: UIImage(named: "ic_home_profile"),
-                                                style: .plain,
-                                                target: self,
-                                                action: nil)
-        navigationItem.leftBarButtonItem = barLeftButtonItem
-        navigationItem.leftBarButtonItem?.tintColor = .black
-        
-        let barRightButtonItem = UIBarButtonItem(image: UIImage(named: "ic_home_chat"),
-                                                 style: .plain,
-                                                 target: self,
-                                                 action: nil)
-        navigationItem.rightBarButtonItem = barRightButtonItem
-        navigationItem.rightBarButtonItem?.tintColor = .black
     }
     
     func setupMapConstraints() {
