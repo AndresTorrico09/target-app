298029a2baa5fd42adda3033fd4d85ea0f178708
andresleoneltorricocossio <andreslionel09@gmail.com>
2022-09-13 19:57:27 +0100

    add create target UI and icon

--- target-app/Home/Views/TargetBottomSheetViewController.swift
+++ target-app/Home/Views/TargetBottomSheetViewController.swift
@@ -8,11 +8,42 @@
 import UIKit
 
 class TargetBottomSheetViewController: UIViewController {
-
+    
+    private var targetImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named:"ic_create_target")
+        return imageView
+    }()
+    
+    private var titleLabel = UILabel(style: .secondary(text: "create_target_label".localized))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         applyBottomSheetUIConfigs()
-    }
+        
+        view.addSubviews(subviews: [
+            targetImage,
+            titleLabel
+        ])
+        
+        targetImage.centerHorizontally(with: view)
 
+        titleLabel.attachHorizontally(
+            to: view,
+            leadingMargin: UI.Defaults.margin,
+            trailingMargin: UI.Defaults.margin
+        )
+        
+        NSLayoutConstraint.activate([
+            targetImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 12),
+            titleLabel.topAnchor.constraint(
+                equalTo: targetImage.bottomAnchor,
+                constant: 20
+            )
+        ])
+
+    }
+    
 }
