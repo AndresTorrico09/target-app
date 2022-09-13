7f2c3e260364346cc127e7ee5265e602b7354933
andresleoneltorricocossio <andreslionel09@gmail.com>
2022-09-13 19:58:24 +0100

    fix navigation bar not showing

--- target-app/Home/Views/HomeContainerViewController.swift
+++ target-app/Home/Views/HomeContainerViewController.swift
@@ -9,10 +9,28 @@ import UIKit
 
 final class HomeContainerViewController: BottomSheetContainerViewController <HomeViewController, TargetBottomSheetViewController> {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewDidAppear(_ animated: Bool) {
+        setupNavigationBar()
+    }
+    
+    func setupNavigationBar() {
+        navigationController?.setNavigationBarHidden(false, animated: true)
         
-        // Do something
+        navigationItem.titleView = UILabel(style: .secondary(text: "home_title".localized))
+        
+        let barLeftButtonItem = UIBarButtonItem(image: UIImage(named: "ic_home_profile"),
+                                                style: .plain,
+                                                target: self,
+                                                action: nil)
+        navigationItem.leftBarButtonItem = barLeftButtonItem
+        navigationItem.leftBarButtonItem?.tintColor = .black
+        
+        let barRightButtonItem = UIBarButtonItem(image: UIImage(named: "ic_home_chat"),
+                                                 style: .plain,
+                                                 target: self,
+                                                 action: nil)
+        navigationItem.rightBarButtonItem = barRightButtonItem
+        navigationItem.rightBarButtonItem?.tintColor = .black
     }
     
 }
