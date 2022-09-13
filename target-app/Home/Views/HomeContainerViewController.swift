9da7c4b3af2118c55956f4f6e254105ab27a3831
andresleoneltorricocossio <andreslionel09@gmail.com>
2022-09-13 16:02:11 +0100

    add container bottom sheet custom

---
+++ target-app/Home/Views/HomeContainerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeContainerViewController.swift
+//  target-app
+//
+//  Created by Andres Leonel Torrico Cossio on 12/09/2022.
+//
+
+import UIKit
+
+final class HomeContainerViewController: BottomSheetContainerViewController <HomeViewController, TargetBottomSheetViewController> {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do something
+    }
+    
+}
---
+++ target-app/Home/Views/TargetBottomSheetViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TargetBottomSheetViewController.swift
+//  target-app
+//
+//  Created by Andres Leonel Torrico Cossio on 12/09/2022.
+//
+
+import UIKit
+
+class TargetBottomSheetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        applyBottomSheetUIConfigs()
+    }
+
+}
