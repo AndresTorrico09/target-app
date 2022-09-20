825c2da1cf4df1ff8c9fe67a4ec298fc12f75842
andresleoneltorricocossio <andreslionel09@gmail.com>
2022-09-20 19:53:24 +0100

    fix UIBarButtonItem indentation

--- target-app/Home/Views/HomeViewController.swift
+++ target-app/Home/Views/HomeViewController.swift
@@ -66,17 +66,21 @@ class HomeViewController: UIViewController, CLLocationManagerDelegate, MKMapView
         
         navigationItem.title = "home_title".localized
         
-        let barLeftButtonItem = UIBarButtonItem(image: UIImage(named: "ic_home_profile"),
-                                                style: .plain,
-                                                target: self,
-                                                action: nil)
+        let barLeftButtonItem = UIBarButtonItem(
+            image: UIImage(named: "ic_home_profile"),
+            style: .plain,
+            target: self,
+            action: nil
+        )
         navigationItem.leftBarButtonItem = barLeftButtonItem
         navigationItem.leftBarButtonItem?.tintColor = .black
         
-        let barRightButtonItem = UIBarButtonItem(image: UIImage(named: "ic_home_chat"),
-                                                 style: .plain,
-                                                 target: self,
-                                                 action: nil)
+        let barRightButtonItem = UIBarButtonItem(
+            image: UIImage(named: "ic_home_chat"),
+            style: .plain,
+            target: self,
+            action: nil
+        )
         navigationItem.rightBarButtonItem = barRightButtonItem
         navigationItem.rightBarButtonItem?.tintColor = .black
     }
