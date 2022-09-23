229d93582b6a32406fb7b3cd52663629d6049e22
Andres Torrico <andreslionel09@gmail.com>
2022-09-23 22:18:41 +0100

    send textfield values to target api using mock location and topics

--- target-app/Home/ViewModels/SaveTargetViewModel.swift
+++ target-app/Home/ViewModels/SaveTargetViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CoreLocation
 
 class SaveTargetViewModel {
     
@@ -16,11 +17,17 @@ class SaveTargetViewModel {
     var statePublisher: Published<AuthViewModelState>.Publisher { $state }
     
     //TODO: remove mock data
-    private var title: String = "new target"
-    private var latitude: Double = -94.5566
-    private var longitude: Double = -94.5566
-    private var radius: Double = 27384.4
-    private var topicId: Int = 2
+    private var title: String = ""
+    private var latitude: Double = 0.0
+    private var longitude: Double = 0.0
+    private var radius: Double = 0.0
+    private var topicId: Int = 0
+    
+    private var location: CLLocation
+    
+    init(location: CLLocation) {
+        self.location = location
+    }
     
     func saveTarget() {
         state = .network(state: .loading)
@@ -41,5 +48,36 @@ class SaveTargetViewModel {
                 self.state = .network(state: .error(error.localizedDescription))
             }
         }
+    }
+    
+    func setTitle(title: String) {
+        self.title = title
+    }
+    
+    func setLocationSelected() {
+        self.latitude = self.location.coordinate.latitude
+        self.longitude = self.location.coordinate.longitude
+    }
+    
+    func setArea(radius: String) {
+        self.radius = Double(radius)!
+    }
+    
+    func setTopicId(topicId: String) {
+        //TODO: get topic values from API
+        let topic = {
+            switch topicId {
+            case "Football":
+                return 1
+            case "Pizza":
+                return 2
+            case "Dogs":
+                return 3
+            default:
+                return 0
+            }
+        }()
+        
+        self.topicId = topic
     }
 }
