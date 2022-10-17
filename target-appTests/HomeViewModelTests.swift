efddfe7c8b73e8e7d2fb82affd272785267bcc5d
Andres Torrico <andreslionel09@gmail.com>
2022-10-17 16:48:02 +0100

    add get target test

---
+++ target-appTests/HomeViewModelTests.swift
@@ -0,0 +1,41 @@
+//
+//  HomeViewModelTests.swift
+//  target-appTests
+//
+//  Created by Andres Leonel Torrico Cossio on 17/10/2022.
+//
+
+import XCTest
+import Combine
+@testable import target_app
+
+class HomeViewModelTests: XCTestCase {
+    
+    var viewModel: HomeViewModel!
+    private var cancellables: Set<AnyCancellable>!
+
+    override func setUp() {
+        super.setUp()
+        viewModel = HomeViewModel(
+            targetServices: TargetServicesMock()
+        )
+        cancellables = []
+    }
+
+    func testGetTargets() {
+        let expect = expectation(description: "get targets")
+        
+        viewModel.$targets
+            .dropFirst()
+            .sink { targets in
+                XCTAssertNotNil(targets)
+                XCTAssertEqual(targets.count, 2)
+                expect.fulfill()
+            }
+            .store(in: &cancellables)
+        
+        viewModel.getTargets()
+        
+        waitForExpectations(timeout: 1)
+    }
+}
