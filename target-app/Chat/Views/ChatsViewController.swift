86a5715f741e19c84d422bcaea1aadf070cc941f
Andres Torrico <andreslionel09@gmail.com>
2022-10-19 17:42:32 +0100

    add navigation bar, add back navigation to home map

--- target-app/Chat/Views/ChatsViewController.swift
+++ target-app/Chat/Views/ChatsViewController.swift
@@ -11,7 +11,41 @@ class ChatsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        applyDefaultUIConfigs()
+        setupNavigationBar()
 
+    }
+    
+    func setupNavigationBar() {
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        
+        navigationItem.titleView = UILabel(style: .secondary(text: "Chat"))
+        
+        let barLeftButtonItem = UIBarButtonItem(
+            image: UIImage(named: "ic_home_profile"),
+            style: .plain,
+            target: self,
+            action: nil
+        )
+        navigationItem.leftBarButtonItem = barLeftButtonItem
+        navigationItem.leftBarButtonItem?.tintColor = .black
+        
+        let barRightButtonItem = UIBarButtonItem(
+            image: UIImage(named: "ic_chat_map"),
+            style: .plain,
+            target: self,
+            action: #selector(tapOnMapBarButton)
+        )
+        navigationItem.rightBarButtonItem = barRightButtonItem
+        navigationItem.rightBarButtonItem?.tintColor = .black
+    }
+    
+    // MARK: - Actions
+
+    @objc
+    func tapOnMapBarButton(_ sender: Any) {
+        AppNavigator.shared.navigate(to: HomeRoutes.home, with: .changeRoot)
     }
 
 }
